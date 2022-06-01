8e81630362b705949b8dd1e172ad9aa168e77925
ShonaliRomeshika <shonaliromeshika05@gmail.com>
2022-06-01 22:04:15 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import { ChatEngine } from 'react-chat-engine';
-
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
 import './App.css';
 
 const App = () => {
+
+    if (!localStorage.getItem('username')) return <LoginForm />;
 
     return(
         <ChatEngine
@@ -10,6 +13,8 @@ const App = () => {
             projectID = "62dc84cf-8c8b-4ea4-bfb4-e34d48e3dd33"
             userName = "Shonali (Admin)"
             userSecret = "123123"
+            renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+            onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
         />
     );
 }
